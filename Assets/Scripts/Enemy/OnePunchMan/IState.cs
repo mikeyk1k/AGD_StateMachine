b0f15557f83c8dcd4faaab62079b4fbd9daad3b1
Mikey <mikeylearn430@gmail.com>
2025-10-21 23:05:08 +0700

    Convert hardcode to State Machine

---
+++ Assets/Scripts/Enemy/OnePunchMan/IState.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace StatePattern.Enemy
+{
+    public interface IState
+    {
+        public OnePunchManController Owner { get; set; }
+        public void OnStateEnter();
+        public void Update();
+        public void OnStateExit();
+    }
+}
---
+++ Assets/Scripts/Enemy/OnePunchMan/States/ShootingState.cs
@@ -0,0 +1,52 @@
+﻿using StatePattern.Main;
+using StatePattern.Player;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace StatePattern.Enemy
+{
+    public class ShootingState : IState
+    {
+        public OnePunchManController Owner {get; set; }
+        private OnePunchManStateMachine stateMachine;
+        private PlayerController target;
+        private float shootTimer;
+
+        public ShootingState(OnePunchManStateMachine stateMachine) => this.stateMachine = stateMachine;
+        public void OnStateEnter()
+        {
+            SetTarget();
+            shootTimer = 0;
+        }
+        public void Update()
+        {
+            Quaternion desiredQuaternion = CalculateRotationTowardsPlayer();
+            Owner.SetRotation(RotateTowards(desiredQuaternion));
+
+            if (IsFacingPlayer(desiredQuaternion))
+            {
+                shootTimer -= Time.deltaTime;
+                if (shootTimer <= 0)
+                {
+                    shootTimer = Owner.Data.RateOfFire;
+                    Owner.Shoot();
+                }
+            }
+        }
+        public void OnStateExit() => target = null;
+
+        private void SetTarget() => target = GameService.Instance.PlayerService.GetPlayer();
+        private Quaternion CalculateRotationTowardsPlayer()
+        {
+            Vector3 directionToPlayer = target.Position - Owner.Position;
+            directionToPlayer.y = 0f;
+            return Quaternion.LookRotation(directionToPlayer, Vector3.up);
+        }
+        private Quaternion RotateTowards(Quaternion desiredRotation) => 
+            Quaternion.LerpUnclamped(Owner.Rotation, desiredRotation, 
+                Owner.Data.RotationSpeed / 30 * Time.deltaTime);
+        private bool IsFacingPlayer(Quaternion desiredRotation) => 
+            Quaternion.Angle(Owner.Rotation, desiredRotation) < Owner.Data.RotationThreshold;
+    }
+}
